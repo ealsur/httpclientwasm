9d1ccd46c4769a2a34929e48ba08225c02ce40bc
Matias Quaranta <maquaran@microsoft.com>
2020-09-24 09:41:42 -0700

    source

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient httpclient = new HttpClient();
+
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://www.bing.com");
+
+            HttpResponseMessage responseMessage = await httpclient.SendAsync(
+                requestMessage,
+                HttpCompletionOption.ResponseHeadersRead);
+
+            Console.WriteLine("Is null" + responseMessage.RequestMessage == null);
+        }
+    }
+}
